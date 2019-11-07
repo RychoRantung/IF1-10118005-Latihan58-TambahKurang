a528b83a20c3a24d8faad424759673d12cfa739c
Rycho Rantung <55700409+RychoRantung@users.noreply.github.com>
2019-11-07 18:34:10 +0700

    Add files via upload

---
+++ IF1-10118005-Latihan58-TambahKurang/src/if1/pkg10118005/latihan58/tambahkurang/IF110118005Latihan58TambahKurang.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118005.latihan58.tambahkurang;
+
+/**
+ *
+ * @author USER
+ */
+public class IF110118005Latihan58TambahKurang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       JumlahBilangan jb = new JumlahBilangan();
+       SelisihBilangan sb = new SelisihBilangan();
+       jb.tampilHasilJumlah();
+       sb.tampilSelisih();
+    }
+    
+}
